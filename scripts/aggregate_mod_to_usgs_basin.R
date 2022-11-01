bdfa120651883539a0b4eaff12e7c23bf69355be
Alex Cebulski <alexcebulski@gmail.com>
2022-11-01 17:28:03 -0600

    update scripts to work with new basins from 2022

--- scripts/aggregate_mod_to_usgs_basin.R
+++ scripts/aggregate_mod_to_usgs_basin.R
@@ -6,13 +6,8 @@ library(sf)
 library(ggplot2)
 library(plotly)
 
-
-path = 'D:/transboundary_hakai/amrit/'
-setwd(path)
-
-
 # get cus 
-dcwbm_grid_new <- st_read("CUS_Runoff_Gridded.csv", 
+dcwbm_grid_new <- st_read('data/modelled/raw from amrit/DCWBM/DCWBM/CUS_Runoff_Gridded.csv', 
                           options=c("AUTODETECT_TYPE=YES", 
                                     "X_POSSIBLE_NAMES=Long",
                                     "Y_POSSIBLE_NAMES=Lat")) 
@@ -20,30 +15,31 @@ dcwbm_grid_new <- st_read("CUS_Runoff_Gridded.csv",
 st_crs(dcwbm_grid_new) <- 4326
 
 # get usgs gauged basins
-usgs_bsn_q <- st_read("../scratch/unreg_Gauged_Data/usgs_data_croix/cus_usgs_basins_huc_IP_ID.shp") %>% 
-  st_transform(st_crs(4326)) 
+usgs_bsn_q <- read.csv('data/gauged/usgs/usgs_1981_2010_monthly_flows_long.csv') 
 
-usgs_bsn_sptl <- usgs_bsn_q %>% 
-  select(OBJECTID, AREA) #just keep spatial for join purposes
+usgs_bsn_sptl <- st_read('data/gis/usgs_gauge_basins/usgs_unregulated_basins_1981-2010_dataRetrieval_2022_update.shp') |> 
+  st_transform(st_crs(4326)) |> 
+  select(GAGE_ID, AREA) #just keep spatial for join purposes
 
-usgs_bsn_area <- usgs_bsn_q %>% 
-  select(OBJECTID, AREA, Name = Station_Na) %>%  #just keep spatial for join purposes
+usgs_bsn_area <- usgs_bsn_sptl %>%
+  select(GAGE_ID, AREA) %>%  #just keep spatial for join purposes
   st_drop_geometry()
+
 usgs_bsn_df <- usgs_bsn_q %>% 
   st_drop_geometry()
 
-write.csv(usgs_bsn_df, "Gauged Compare/usgs_gaugeDat.csv")
+# write.csv(usgs_bsn_df, "Gauged Compare/usgs_gaugeDat.csv")
 
 join <- st_join(dcwbm_grid_new, usgs_bsn_sptl, join = st_intersects) %>% 
   st_drop_geometry()
 
 colnames(join)[5:16] <- c('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
 
-dcwbm_grid_new <- NULL
+# dcwbm_grid_new <- NULL
 
 pts.avg <- join %>% 
-  filter(is.na(OBJECTID) == F) %>% 
-  group_by(OBJECTID) %>% 
+  filter(is.na(GAGE_ID) == F) %>% 
+  group_by(GAGE_ID) %>% 
   summarize(Jan = mean(Jan, na.rm = T),
             Feb = mean(Feb, na.rm = T),
             Mar = mean(Mar, na.rm = T),
@@ -58,14 +54,14 @@ pts.avg <- join %>%
             Dec = mean(Dec, na.rm = T)) %>%
   mutate(ann_mm = rowSums(.[,2:13, drop=TRUE], na.rm = TRUE)) 
 
-#write.csv(pts.avg, "Gauged Compare/dcwbm2020_usgsPolyUnreg.csv")
+write.csv(pts.avg, "data/modelled/dcwbm_raw_from_amrit_usgs_unreg_1981_2010.csv", row.names = F)
 
-pts.avg <- read.csv("Gauged Compare/dcwbm2020_usgsPolyUnreg.csv") %>% select(OBJECTID:ann_mm)
+pts.avg <- read.csv("Gauged Compare/dcwbm2020_usgsPolyUnreg.csv")
 
 ###### convert to m3/s##### 
 
 dcwbm_mm <- pts.avg %>% 
-  left_join(usgs_bsn_area, by = "OBJECTID")
+  left_join(usgs_bsn_area, by = "GAGE_ID")
 
 colnames(dcwbm_mm)[2:13] <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
 
@@ -74,7 +70,7 @@ mdays = data.frame(Month = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12), mdays = c(3
 
 dcwbm_long <- dcwbm_mm %>% 
   pivot_longer(`1`:`12`, names_to = "Month", values_to = "q") %>% 
-  mutate(ID = paste0(OBJECTID, Month))
+  mutate(ID = paste0(GAGE_ID, Month))
 
 # coherce months to numeric
 dcwbm_long$Month <- as.numeric(dcwbm_long$Month)
@@ -85,133 +81,133 @@ dcwbm_cms <- dcwbm_long %>%
   mutate(q_cms =  (((q/1000) * (AREA)) * (1/(mdays*24*60*60))),
          q_cky = (q/1000000) * (AREA/1000000))
 
-write.csv(dcwbm_cms, "Gauged Compare/cus_dcwbm_long_mm_cms_cky.csv")
+write.csv(dcwbm_cms, "data/modelled/dcwbm_raw_from_amrit_usgs_unreg_1981_2010_cky.csv")
 
 # gauge data
-usgs_bsn_df <- usgs_bsn_q %>% 
-  st_drop_geometry() %>% 
-  select(OBJECTID, Jan:Dec, area = Shape_Area, regime, Name = Station_Na)
-
-colnames(usgs_bsn_df)[2:13] <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
-
-usgs_long <- usgs_bsn_df %>% 
-  pivot_longer(`1`:`12`, names_to = "Month", values_to = "q") %>% 
-  mutate(ID = paste0(OBJECTID, Month))
-
-# coherce months to numeric
-usgs_long$Month <- as.numeric(usgs_long$Month)
-
-# convert mm to cms 
-usgs_cms <- usgs_long %>% 
-  left_join(mdays, by = 'Month') %>% 
-  mutate(q_cms =  (((q/1000) * (1000000 * area)) * (1/(mdays*24*60*60))),
-         q_cky = (q/1000000) * (area/1000000))
-
-write.csv(usgs_cms, "Gauged Compare/usgs_unreg_long_mm_cms_cky.csv")
-
-# get usgs area in km3 
-wsc_area <- wsc_bsn_q %>% 
-  st_drop_geometry() %>% 
-  select(WSC, area)
-
-#### plot ####
-usgs_mod <- read.csv("Gauged Compare/dcwbm2020_usgsPolyUnreg.csv")
-usgs_gauge <- read.csv("Gauged Compare/usgs_gaugeDat.csv") %>%
-  filter(NDAMS_2009 == 0, OBJECTID != 24) # set to unregulated - and remove clark basin because some sort of damming must be going on for those high summer flows. 
-
-
-# monthly
-mod_long <- usgs_mod %>% 
-  pivot_longer(Jan:Dec, names_to = "Month", values_to = "mod_q") %>% 
-  mutate(ID = paste0(OBJECTID, Month))  
-
-usgs_long <- usgs_gauge %>% 
-  #st_drop_geometry() %>% 
-  select(OBJECTID, Station_Na, Jan:Dec, regime) %>% 
-  pivot_longer(Jan:Dec, names_to = "Month", values_to = "wsc_q")%>% 
-  mutate(ID = paste0(OBJECTID, Month))  
-
-combine <- left_join(usgs_long, mod_long, suffix = c("_obs", "_mod"), by = "ID")  
-
-ggplot(combine, aes(x= wsc_q, y = mod_q)) +
-  geom_point(aes(colour = factor(regime))) +
-  labs(x='Observed (mm)', y = 'Modelled (mm)', colour = "Regime:") +
-  geom_abline(intercept = 0, slope = 1) +
-  xlim(0, 600) +
-  ylim(0, 600) +
-  scale_colour_brewer(palette="Dark2") +
-  theme_bw() 
-
-ggplot(combine, aes(x= wsc_q, y = mod_q)) +
-  geom_point(aes(colour = factor(OBJECTID_obs), shape = factor(regime))) +
-  labs(x='Observed (mm)', y = 'Modelled (mm)', colour = "Basin ID:", shape = "Regime:") +
-  geom_abline(intercept = 0, slope = 1) +
-  xlim(0, 600) +
-  ylim(0, 600) +
-  scale_colour_brewer(palette="Dark2") +
-  theme_bw() 
-
-ggsave('Gauged Compare/cus_monthly_unregulated_amrit2020_2.png', width = 8, height = 6, units = "in")
-
-plot_ly(
-  data = combine, 
-  x = ~wsc_q, y = ~mod_q, 
-  type = "scatter", 
-  mode = "markers", 
-  color = ~regime,
-  text = ~Station_Na,
-  hovertemplate = paste(
-    "<b>%{text}</b><br>",
-    "%{yaxis.title.text}: %{y:,.0f}<br>",
-    "%{xaxis.title.text}: %{x:,.0f}")) %>% 
-  layout(
-    xaxis = list(title = "Observed (mm)"),
-    yaxis = list(title = "Modelled (mm)")
-  )
-
-
-# annual 
-
-# plot annual mm
-usgs_ann <- usgs_gauge %>% 
-  #st_drop_geometry() %>% 
-  select(Station_Na, OBJECTID, ann_mm, regime, NDAMS_2009)
-
-mod_ann <- usgs_mod %>% 
-  select(OBJECTID, ann_mm_mod = ann_mm)
-
-cus_combine_ann <- left_join(usgs_ann, mod_ann, by = "OBJECTID") %>% 
-  filter(NDAMS_2009 == 0, OBJECTID != 24) %>% # set to unregulated - and remove clark basin because some sort of damming must be going on for those high summer flows.
-  mutate(diff = (ann_mm - ann_mm_mod)) %>% 
-  mutate(perc_diff = (ann_mm - ann_mm_mod)/ann_mm_mod)  
-
-ggplot(combine_ann, aes(x= ann_mm, y = ann_mm_mod)) +
-  geom_point(aes(colour = factor(regime))) +
-  labs(x='Observed (mm)', y = 'Modelled (mm)', colour = "Regime:") +
-  geom_abline(intercept = 0, slope = 1) +
-  xlim(0, 4500) +
-  ylim(0, 4500) +
-  scale_colour_brewer(palette="Dark2") +
-  theme_bw() 
-
-ggsave('Gauged Compare/cus_annual_unregulated_amrit2020.png', width = 8, height = 6, units = "in")
-
-
-plot_ly(
-  data = combine_ann, 
-  x = ~ann_mm, y = ~ann_mm_mod, 
-  type = "scatter", 
-  mode = "markers", 
-  color = ~regime,
-  text = ~Station_Na,
-  hovertemplate = paste(
-    "<b>%{text}</b><br>",
-    "%{yaxis.title.text}: %{y:,.0f}<br>",
-    "%{xaxis.title.text}: %{x:,.0f}")) %>% 
-  layout(
-    xaxis = list(title = "Observed (mm)"),
-    yaxis = list(title = "Modelled (mm)")
-  )
-
-
-
+# usgs_bsn_df <- usgs_bsn_q %>% 
+#   st_drop_geometry() %>% 
+#   select(GAGE_ID, Jan:Dec, area = Shape_Area, regime, Name = Station_Na)
+# 
+# colnames(usgs_bsn_df)[2:13] <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+# 
+# usgs_long <- usgs_bsn_df %>% 
+#   pivot_longer(`1`:`12`, names_to = "Month", values_to = "q") %>% 
+#   mutate(ID = paste0(GAGE_ID, Month))
+# 
+# # coherce months to numeric
+# usgs_long$Month <- as.numeric(usgs_long$Month)
+# 
+# # convert mm to cms 
+# usgs_cms <- usgs_long %>% 
+#   left_join(mdays, by = 'Month') %>% 
+#   mutate(q_cms =  (((q/1000) * (1000000 * area)) * (1/(mdays*24*60*60))),
+#          q_cky = (q/1000000) * (area/1000000))
+# 
+# write.csv(usgs_cms, "Gauged Compare/usgs_unreg_long_mm_cms_cky.csv")
+# 
+# # get usgs area in km3 
+# wsc_area <- wsc_bsn_q %>% 
+#   st_drop_geometry() %>% 
+#   select(WSC, area)
+# 
+# #### plot ####
+# usgs_mod <- read.csv("Gauged Compare/dcwbm2020_usgsPolyUnreg.csv")
+# usgs_gauge <- read.csv("Gauged Compare/usgs_gaugeDat.csv") %>%
+#   filter(NDAMS_2009 == 0, GAGE_ID != 24) # set to unregulated - and remove clark basin because some sort of damming must be going on for those high summer flows. 
+# 
+# 
+# # monthly
+# mod_long <- usgs_mod %>% 
+#   pivot_longer(Jan:Dec, names_to = "Month", values_to = "mod_q") %>% 
+#   mutate(ID = paste0(GAGE_ID, Month))  
+# 
+# usgs_long <- usgs_gauge %>% 
+#   #st_drop_geometry() %>% 
+#   select(GAGE_ID, Station_Na, Jan:Dec, regime) %>% 
+#   pivot_longer(Jan:Dec, names_to = "Month", values_to = "wsc_q")%>% 
+#   mutate(ID = paste0(GAGE_ID, Month))  
+# 
+# combine <- left_join(usgs_long, mod_long, suffix = c("_obs", "_mod"), by = "ID")  
+# 
+# ggplot(combine, aes(x= wsc_q, y = mod_q)) +
+#   geom_point(aes(colour = factor(regime))) +
+#   labs(x='Observed (mm)', y = 'Modelled (mm)', colour = "Regime:") +
+#   geom_abline(intercept = 0, slope = 1) +
+#   xlim(0, 600) +
+#   ylim(0, 600) +
+#   scale_colour_brewer(palette="Dark2") +
+#   theme_bw() 
+# 
+# ggplot(combine, aes(x= wsc_q, y = mod_q)) +
+#   geom_point(aes(colour = factor(GAGE_ID_obs), shape = factor(regime))) +
+#   labs(x='Observed (mm)', y = 'Modelled (mm)', colour = "Basin ID:", shape = "Regime:") +
+#   geom_abline(intercept = 0, slope = 1) +
+#   xlim(0, 600) +
+#   ylim(0, 600) +
+#   scale_colour_brewer(palette="Dark2") +
+#   theme_bw() 
+# 
+# ggsave('Gauged Compare/cus_monthly_unregulated_amrit2020_2.png', width = 8, height = 6, units = "in")
+# 
+# plot_ly(
+#   data = combine, 
+#   x = ~wsc_q, y = ~mod_q, 
+#   type = "scatter", 
+#   mode = "markers", 
+#   color = ~regime,
+#   text = ~Station_Na,
+#   hovertemplate = paste(
+#     "<b>%{text}</b><br>",
+#     "%{yaxis.title.text}: %{y:,.0f}<br>",
+#     "%{xaxis.title.text}: %{x:,.0f}")) %>% 
+#   layout(
+#     xaxis = list(title = "Observed (mm)"),
+#     yaxis = list(title = "Modelled (mm)")
+#   )
+# 
+# 
+# # annual 
+# 
+# # plot annual mm
+# usgs_ann <- usgs_gauge %>% 
+#   #st_drop_geometry() %>% 
+#   select(Station_Na, GAGE_ID, ann_mm, regime, NDAMS_2009)
+# 
+# mod_ann <- usgs_mod %>% 
+#   select(GAGE_ID, ann_mm_mod = ann_mm)
+# 
+# cus_combine_ann <- left_join(usgs_ann, mod_ann, by = "GAGE_ID") %>% 
+#   filter(NDAMS_2009 == 0, GAGE_ID != 24) %>% # set to unregulated - and remove clark basin because some sort of damming must be going on for those high summer flows.
+#   mutate(diff = (ann_mm - ann_mm_mod)) %>% 
+#   mutate(perc_diff = (ann_mm - ann_mm_mod)/ann_mm_mod)  
+# 
+# ggplot(combine_ann, aes(x= ann_mm, y = ann_mm_mod)) +
+#   geom_point(aes(colour = factor(regime))) +
+#   labs(x='Observed (mm)', y = 'Modelled (mm)', colour = "Regime:") +
+#   geom_abline(intercept = 0, slope = 1) +
+#   xlim(0, 4500) +
+#   ylim(0, 4500) +
+#   scale_colour_brewer(palette="Dark2") +
+#   theme_bw() 
+# 
+# ggsave('Gauged Compare/cus_annual_unregulated_amrit2020.png', width = 8, height = 6, units = "in")
+# 
+# 
+# plot_ly(
+#   data = combine_ann, 
+#   x = ~ann_mm, y = ~ann_mm_mod, 
+#   type = "scatter", 
+#   mode = "markers", 
+#   color = ~regime,
+#   text = ~Station_Na,
+#   hovertemplate = paste(
+#     "<b>%{text}</b><br>",
+#     "%{yaxis.title.text}: %{y:,.0f}<br>",
+#     "%{xaxis.title.text}: %{x:,.0f}")) %>% 
+#   layout(
+#     xaxis = list(title = "Observed (mm)"),
+#     yaxis = list(title = "Modelled (mm)")
+#   )
+# 
+# 
+# 
--- scripts/aggregate_mod_to_wsc_basin.R
+++ scripts/aggregate_mod_to_wsc_basin.R
@@ -6,56 +6,29 @@ library(sf)
 library(ggplot2)
 library(plotly)
 
-path = 'D:/transboundary_hakai/Scratch/unreg_Gauged_Data'
-setwd(path)
+# get pts not including fraser or cus
+dcwbm_grid_new <- st_read("data/modelled/raw from amrit/DCWBM/DCWBM/HUC_Runoff_Gridded.csv",
+                          options=c("AUTODETECT_TYPE=YES",
+                                    "X_POSSIBLE_NAMES=Long",
+                                    "Y_POSSIBLE_NAMES=Lat"))
+# set to wgs84
+st_crs(dcwbm_grid_new) <- 4326
 
+# bring in update gauged delins 
 
-# # get pts not including fraser or cus
-# dcwbm_grid_new <- st_read("D:/transboundary_hakai/amrit/HUC_Runoff_Gridded.csv", 
-#                           options=c("AUTODETECT_TYPE=YES", 
-#                                     "X_POSSIBLE_NAMES=Long",
-#                                     "Y_POSSIBLE_NAMES=Lat")) 
-# # set to wgs84
-# st_crs(dcwbm_grid_new) <- 4326
-
-# downsampled to 50 m 
-dcwbm_grid_new <- st_read("dcwbm_pts_50m_6gauges.shp")
-
-saveRDS(dcwbm_grid_new, "dcbwmPoints_50m.rds")
-
-# points only have IDs join to flow depth later
-dcwbm_grid_new <- readRDS("dcbwmPoints_50m.rds")
-
-# get wsc basins that are unregulated from joels original dataset 
-# wsc_bsn_q <- st_read("../scratch/unreg_Gauged_Data/wsc_data/WSC_polygons_qGaged_IP_IDs.shp") %>% 
-#   st_transform(st_crs(4326)) 
-
-# updated basins 2021
-# nhc_meta <- read.csv("D:/transboundary_hakai/scratch/WSC_Basins_NHC_2021/Appendix 5.3 GIS Data/20200817_3004476_NHC_station_metadata.R0.csv") %>% 
-#   dplyr::rename(ID = Gauge.ID)
-# 
-# nhc_bsns <- st_read("D:/transboundary_hakai/scratch/WSC_Basins_NHC_2021/Appendix 5.3 GIS Data/20200811_3004476_NHC_all_watersheds.R0.shp") %>% 
-#   left_join(nhc_meta) %>% 
-#   st_transform(4326)
-
-# combo of nhc, foundry and env can
-wsc_bsns1 <- sf::st_read("D:/transboundary_hakai/scratch/unreg_Gauged_Data/2021_UPDATED_wsc_basins.shp")
-wsc_bsns2 <- sf::st_read("D:/transboundary_hakai/scratch/unreg_Gauged_Data/2021_UPDATED_wsc_basins.shp") %>% slice(50:n())
+wsc_bsns <- read_sf('data/gis/wsc_gauge_basins/hnbp_2022/hnbp_2022_wsc_basins_w_dat_avail.shp') |> 
+  st_transform(st_crs(dcwbm_grid_new)) |> 
+  st_make_valid()
 
 # join basin ID to points 
-dcwbm_grid_new <- st_join(dcwbm_grid_new, wsc_bsns1, join = st_intersects) %>%
+dcwbm_grid_new_jn <- st_join(dcwbm_grid_new, wsc_bsns, join = st_intersects) %>%
   st_drop_geometry()
 
-# runoff data to join to points
-flow_depths <- read_csv("D:/transboundary_hakai/amrit/HUC_Runoff_Gridded.csv")
+colnames(dcwbm_grid_new_jn)[5:16] <- c('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
 
-colnames(join)[5:16] <- c('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
-
-dcwbm_grid_new <- NULL
-
-pts.avg <- join %>%
-  filter(is.na(ID.y) == F) %>%
-  group_by(ID.y) %>%
+pts.avg <- dcwbm_grid_new_jn %>%
+  filter(is.na(StationNum) == F) %>%
+  group_by(StationNum) %>%
   summarize(Jan = mean(Jan, na.rm = T),
             Feb = mean(Feb, na.rm = T),
             Mar = mean(Mar, na.rm = T),
@@ -69,11 +42,11 @@ pts.avg <- join %>%
             Nov = mean(Nov, na.rm = T),
             Dec = mean(Dec, na.rm = T)) %>%
   mutate(ann_mm = rowSums(.[,2:13, drop=TRUE], na.rm = TRUE)) %>% 
-  rename(ID = ID.y)
+  rename(ID = StationNum)
 
-write.csv(pts.avg, "2021_dcwbm_nhc_foundry_EC_wsc_usgs.csv", row.names = F)
+write.csv(pts.avg, "data/modelled/wsc/dcwbm_raw_from_amrit_wsc_unreg_1981_2010.csv", row.names = F)
 
-pts.avg <- read.csv("Gauged Compare/dcwbm2020_wscPolyUnreg.csv") %>% select(WSC:ann_mm)
+pts.avg <- read.csv("data/modelled/wsc/dcwbm_raw_from_amrit_wsc_unreg_1981_2010.csv")
 
 
 # ###### convert to m3/s##### 
@@ -144,87 +117,87 @@ pts.avg <- read.csv("Gauged Compare/dcwbm2020_wscPolyUnreg.csv") %>% select(WSC:
 
 # bind wsc and dcwbm to plot mm
 
-mod <- read.csv("2021_dcwbm_nhc_foundry_EC_wsc_usgs.csv")
-gauge <- read.csv("2021_UPDATED_wsc_1981-2010_monthly_normals_long.csv")
-
-
-# monthly plots
-
-month_num <- data.frame(num = c(1:12), month = c('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
-
-dcwbm_long <- mod %>% 
-  pivot_longer(Jan:Dec, names_to = "month", values_to = "dcwbm_q") %>% 
-  left_join(month_num, by = "month") %>% 
-  select(ID, month = num, dcwbm_q)
-
-
-wsc_long <- gauge %>% 
-  select(ID = STATION_NUMBER, month = Month, gauge_q = runoff)
-
-dat <- left_join(wsc_long, dcwbm_long, by = c("ID", "month"))  
-
-ggplot(dat, aes(x= gauge_q, y = dcwbm_q)) +
-  geom_point() +
-  labs(x='Observed (mm)', y = 'Modelled (mm)') +
-  geom_abline(intercept = 0, slope = 1) +
-  xlim(0, 700) +
-  ylim(0, 700) +
-  scale_colour_brewer(palette="Dark2") +
-  theme_bw() 
-
-ggsave('Gauged Compare/bcak_monthly_amrit2020.png', width = 8, height = 6, units = "in")
-
-
-# plot annual mm
-wsc_ann <- read.csv("D:/transboundary_hakai/scratch/unreg_Gauged_Data/2021_UPDATED_wsc_1981-2010_monthly_normals_wide.csv") %>% 
-  select(ID = STATION_NUMBER, ann_mm)
-
-dcbwm_ann <- mod %>% 
-  select(ID, ann_mm_mod = ann_mm)
-
-combine_ann <- left_join(wsc_ann, dcbwm_ann, by = "ID") %>% 
-  mutate(diff = (ann_mm - ann_mm_mod)) %>% 
-  mutate(perc_diff = (ann_mm - ann_mm_mod)/ann_mm_mod)
-
-ggplot(combine_ann, aes(x= ann_mm, y = ann_mm_mod)) +
-  geom_point() +
-  labs(x='Observed (mm)', y = 'Modelled (mm)', colour = "Regime:") +
-  geom_abline(intercept = 0, slope = 1) +
-  xlim(0, 4500) +
-  ylim(0, 4500) +
-  scale_colour_brewer(palette="Dark2") +
-  theme_bw() 
-
-
-plot_ly(
-  data = combine_ann, 
-  x = ~ann_mm, y = ~ann_mm_mod, 
-  type = "scatter", 
-  mode = "markers", 
-  color = ~regime,
-  text = ~Name,
-  hovertemplate = paste(
-    "<b>%{text}</b><br>",
-    "%{yaxis.title.text}: %{y:,.0f}<br>",
-    "%{xaxis.title.text}: %{x:,.0f}")) %>% 
-  layout(
-    xaxis = list(title = "Observed (mm)"),
-    yaxis = list(title = "Modelled (mm)")
-  )
-
-# group by regime 
-combine_cms <- left_join(wsc_cms, dcwbm_cms, suffix = c("_obs", "_mod"), by = "ID")
-
-summarizeRegime <- combine_cms %>% 
-  group_by(regime) %>% 
-  summarise(across(c(q_cms_obs, q_cms_mod), mean))
-
-ggplot(summarizeRegime, aes(x= q_cms_obs, y = q_cms_mod)) +
-  geom_point(aes(colour = factor(regime))) +
-  labs(x='Observed (mm)', y = 'Modelled (mm)', colour = "Regime:") +
-  geom_abline(intercept = 0, slope = 1) +
-  # xlim(0, 4500) +
-  # ylim(0, 4500) +
-  scale_colour_brewer(palette="Dark2") +
-  theme_bw() 
+# mod <- read.csv("2021_dcwbm_nhc_foundry_EC_wsc_usgs.csv")
+# gauge <- read.csv("2021_UPDATED_wsc_1981-2010_monthly_normals_long.csv")
+# 
+# 
+# # monthly plots
+# 
+# month_num <- data.frame(num = c(1:12), month = c('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+# 
+# dcwbm_long <- mod %>% 
+#   pivot_longer(Jan:Dec, names_to = "month", values_to = "dcwbm_q") %>% 
+#   left_join(month_num, by = "month") %>% 
+#   select(ID, month = num, dcwbm_q)
+# 
+# 
+# wsc_long <- gauge %>% 
+#   select(ID = STATION_NUMBER, month = Month, gauge_q = runoff)
+# 
+# dat <- left_join(wsc_long, dcwbm_long, by = c("ID", "month"))  
+# 
+# ggplot(dat, aes(x= gauge_q, y = dcwbm_q)) +
+#   geom_point() +
+#   labs(x='Observed (mm)', y = 'Modelled (mm)') +
+#   geom_abline(intercept = 0, slope = 1) +
+#   xlim(0, 700) +
+#   ylim(0, 700) +
+#   scale_colour_brewer(palette="Dark2") +
+#   theme_bw() 
+# 
+# ggsave('Gauged Compare/bcak_monthly_amrit2020.png', width = 8, height = 6, units = "in")
+# 
+# 
+# # plot annual mm
+# wsc_ann <- read.csv("D:/transboundary_hakai/scratch/unreg_Gauged_Data/2021_UPDATED_wsc_1981-2010_monthly_normals_wide.csv") %>% 
+#   select(ID = STATION_NUMBER, ann_mm)
+# 
+# dcbwm_ann <- mod %>% 
+#   select(ID, ann_mm_mod = ann_mm)
+# 
+# combine_ann <- left_join(wsc_ann, dcbwm_ann, by = "ID") %>% 
+#   mutate(diff = (ann_mm - ann_mm_mod)) %>% 
+#   mutate(perc_diff = (ann_mm - ann_mm_mod)/ann_mm_mod)
+# 
+# ggplot(combine_ann, aes(x= ann_mm, y = ann_mm_mod)) +
+#   geom_point() +
+#   labs(x='Observed (mm)', y = 'Modelled (mm)', colour = "Regime:") +
+#   geom_abline(intercept = 0, slope = 1) +
+#   xlim(0, 4500) +
+#   ylim(0, 4500) +
+#   scale_colour_brewer(palette="Dark2") +
+#   theme_bw() 
+# 
+# 
+# plot_ly(
+#   data = combine_ann, 
+#   x = ~ann_mm, y = ~ann_mm_mod, 
+#   type = "scatter", 
+#   mode = "markers", 
+#   color = ~regime,
+#   text = ~Name,
+#   hovertemplate = paste(
+#     "<b>%{text}</b><br>",
+#     "%{yaxis.title.text}: %{y:,.0f}<br>",
+#     "%{xaxis.title.text}: %{x:,.0f}")) %>% 
+#   layout(
+#     xaxis = list(title = "Observed (mm)"),
+#     yaxis = list(title = "Modelled (mm)")
+#   )
+# 
+# # group by regime 
+# combine_cms <- left_join(wsc_cms, dcwbm_cms, suffix = c("_obs", "_mod"), by = "ID")
+# 
+# summarizeRegime <- combine_cms %>% 
+#   group_by(regime) %>% 
+#   summarise(across(c(q_cms_obs, q_cms_mod), mean))
+# 
+# ggplot(summarizeRegime, aes(x= q_cms_obs, y = q_cms_mod)) +
+#   geom_point(aes(colour = factor(regime))) +
+#   labs(x='Observed (mm)', y = 'Modelled (mm)', colour = "Regime:") +
+#   geom_abline(intercept = 0, slope = 1) +
+#   # xlim(0, 4500) +
+#   # ylim(0, 4500) +
+#   scale_colour_brewer(palette="Dark2") +
+#   theme_bw() 
 
